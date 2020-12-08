2d7d5aa0f3f433d7af561b9cdc88e29bdadf2dad
fernandeslouro <jd.louro@outlook.pt>
2020-12-08 16:34:40 +0000

    add early game, gam over check and winner analysis

--- checkers.py
+++ checkers.py
@@ -15,9 +15,52 @@
  - Values are a list of the positions taken by each type of piece
 
 
-
-
-
-
+== Moves ==
+ - Moves are lists of tuples (lists because a move can have several sub-moves)
+ - Each tuple will be in the form (position of the piece to move, position to move the piece to)
 
 '''
+
+def initial_game():
+    state = {}
+    state['x'] = [x for x in range(1,14) if x%9 != 0]
+    state['-'] = [x for x in range(14,23) if x%9 != 0]
+    state['o'] = [x for x in range(23,36) if x%9 != 0]
+    return state
+
+def game_over(state):
+    for piece, taken_positions in state.items():
+        if (piece in ['o', 'x']) and (len(taken_positions) == 0):
+            return True
+    return False
+
+def winner(state):
+    if not game_over(state):
+        print('Game not over')
+    else:
+        for piece, taken_positions in state.items():
+            if (piece in ['o', 'x']) and (len(taken_positions) != 0):
+                return piece
+
+def computer_play(state, piece):
+    return move
+
+def update_board(state, move, piece):
+    return state
+
+
+# starting game
+state = initial_game()
+print(state)
+while not game_over(state):
+
+    #play computer - ninitially computer will be x piece
+    computer_play(state, 'x')
+    if game_over(state):
+        break
+
+    #play player
+
+
+
+print(f'The winner is {winner(state)}')
