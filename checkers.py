c1c15c78f5fa9569cee5d2454abb303b52e26c01
fernandeslouro <jd.louro@outlook.pt>
2020-12-08 15:49:07 +0000

    first comments - initial thought on how to describe the game:w

---
+++ checkers.py
@@ -0,0 +1,23 @@
+'''
+== Board Positions ==
+
+ 1 . 2 . 3 . 4 .
+ . 5 . 6 . 7 . 8
+10 .11 .12 .13 .
+ .14 .15 .16 .17
+19 .20 .21 .22 .
+ .23 .24 .25 .26
+28 .29 .30 .31 .
+ .32 .33 .34 .35
+
+== Board State Representation
+ - Dictionary with keys 'x', 'o', '-'
+ - Values are a list of the positions taken by each type of piece
+
+
+
+
+
+
+
+'''
