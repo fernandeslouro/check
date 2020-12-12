8f27835bb7afcb31c0e501480ed519b55b95b53b
fernandeslouro <jd.louro@outlook.pt>
2020-12-12 22:02:29 +0000

    made game semi-playable (still cant capture pieces)

--- checkers.py
+++ checkers.py
@@ -1,4 +1,5 @@
 import random
+import copy
 '''
 == Board Positions ==
 
@@ -75,7 +76,7 @@ def update_board(state, move, piece):
     return state
 
 def is_valid_move(state, move, piece):
-    hyp_state = state
+    hyp_state = copy.deepcopy(state)
     for submove in move:
         if is_valid_submove(hyp_state, submove, piece):
             hyp_state = update_board_submove(hyp_state, submove, piece)
@@ -123,33 +124,23 @@ def get_piece_from_position(state, position):
             return piece
 
 
-
-
-
-
-
-
-
-
-
-
-
 def draw_board(state):
     nl = '\n' 
     print(f'\
-     . {get_piece_from_position(state, 1)} . {get_piece_from_position(state, 2)} . {get_piece_from_position(state, 3)} . {get_piece_from_position(state, 4)}     . 1 . 2 . 3 . 4{nl}\
-     {get_piece_from_position(state, 5)} . {get_piece_from_position(state, 6)} . {get_piece_from_position(state, 7)} . {get_piece_from_position(state, 8)} .     5 . 6 . 7 . 8 .{nl}\
-     . {get_piece_from_position(state, 10)} . {get_piece_from_position(state, 11)} . {get_piece_from_position(state, 12)} . {get_piece_from_position(state, 13)}     .10 .11 .12 .13{nl}\
-    {get_piece_from_position(state, 14)} . {get_piece_from_position(state, 15)} . {get_piece_from_position(state, 16)} . {get_piece_from_position(state, 17)}       14 .15 .16 .17 .   {nl}\
-     . {get_piece_from_position(state, 19)} . {get_piece_from_position(state, 20)} . {get_piece_from_position(state, 21)} . {get_piece_from_position(state, 22)}     .19 .20 .21 .22{nl}\
+    . {get_piece_from_position(state, 1)} . {get_piece_from_position(state, 2)} . {get_piece_from_position(state, 3)} . {get_piece_from_position(state, 4)}      . 1 . 2 . 3 . 4{nl}\
+    {get_piece_from_position(state, 5)} . {get_piece_from_position(state, 6)} . {get_piece_from_position(state, 7)} . {get_piece_from_position(state, 8)} .      5 . 6 . 7 . 8 .{nl}\
+    . {get_piece_from_position(state, 10)} . {get_piece_from_position(state, 11)} . {get_piece_from_position(state, 12)} . {get_piece_from_position(state, 13)}      .10 .11 .12 .13{nl}\
+    {get_piece_from_position(state, 14)} . {get_piece_from_position(state, 15)} . {get_piece_from_position(state, 16)} . {get_piece_from_position(state, 17)} .     14 .15 .16 .17 .   {nl}\
+    . {get_piece_from_position(state, 19)} . {get_piece_from_position(state, 20)} . {get_piece_from_position(state, 21)} . {get_piece_from_position(state, 22)}      .19 .20 .21 .22{nl}\
     {get_piece_from_position(state, 23)} . {get_piece_from_position(state, 24)} . {get_piece_from_position(state, 25)} . {get_piece_from_position(state, 26)} .     23 .24 .25 .26 .{nl}\
-     . {get_piece_from_position(state, 28)} . {get_piece_from_position(state, 29)} . {get_piece_from_position(state, 30)} . {get_piece_from_position(state, 31)}     .28 .29 .30 .31{nl}\
+    . {get_piece_from_position(state, 28)} . {get_piece_from_position(state, 29)} . {get_piece_from_position(state, 30)} . {get_piece_from_position(state, 31)}      .28 .29 .30 .31{nl}\
     {get_piece_from_position(state, 32)} . {get_piece_from_position(state, 33)} . {get_piece_from_position(state, 34)} . {get_piece_from_position(state, 35)} .     32 .33 .34 .35 .{nl}\
     ')
 
 
 
 def update_board_submove(state, submove, piece):
+    print(state)
     state[piece].append(submove[1])
     state[piece].remove(submove[0])
     state['-'].append(submove[0])
@@ -175,11 +166,11 @@ while not game_over(board):
     if game_over(board):
         break
 
-    print('==== YOUR MOVE ===')
     #play player
-    move = get_user_move()
-    if is_valid_move(board, move, 'o'):
-        state = update_board(board, move, 'o')
+    while not is_valid_move(board, move, 'o'):
+        print('==== YOUR MOVE ===')
+        move = get_user_move()
 
+    board = update_board(board, move, 'o')
 
 print(f'The winner is {winner(board)}')
